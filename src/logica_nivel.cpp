b5a9d25e06e0cf07457ae5a34c3a8fcd5b5746c9
GUSTAVO ENGEL <gustavo_engel@estudante.sesisenai.org.br>
2026-03-24 20:42:58 -0300

    feat: adicionar trava de seguranca para falha de sensores

--- src/logica_nivel.cpp
+++ src/logica_nivel.cpp
@@ -1,13 +1,37 @@
 #include "hardware.h"
 
 void lerSensores() {
-    if (digitalRead(nivela6) == HIGH) litros = 10000;
-    else if (digitalRead(nivela5) == HIGH) litros = 9000;
-    else if (digitalRead(nivela4) == HIGH) litros = 7000;
-    else if (digitalRead(nivela3) == HIGH) litros = 5000;
-    else if (digitalRead(nivela2) == HIGH) litros = 3000;
-    else if (digitalRead(nivela1) == HIGH) litros = 1000;
-    else litros = 0;
+    // Lendo o estado de todos os pinos primeiro para facilitar a lógica
+    bool n1 = digitalRead(nivela1) == HIGH;
+    bool n2 = digitalRead(nivela2) == HIGH;
+    bool n3 = digitalRead(nivela3) == HIGH;
+    bool n4 = digitalRead(nivela4) == HIGH;
+    bool n5 = digitalRead(nivela5) == HIGH;
+    bool n6 = digitalRead(nivela6) == HIGH;
+
+    // Verificação em cascata: O nível X só é ativado se ele e TODOS abaixo dele estiverem HIGH
+    if (n6 && n5 && n4 && n3 && n2 && n1) {
+        litros = 10000; // 100% (Sobrenível)
+    } 
+    else if (n5 && n4 && n3 && n2 && n1) {
+        litros = 9000;  // 90%
+    } 
+    else if (n4 && n3 && n2 && n1) {
+        litros = 7000;  // 70%
+    } 
+    else if (n3 && n2 && n1) {
+        litros = 5000;  // 50%
+    } 
+    else if (n2 && n1) {
+        litros = 3000;  // 30%
+    } 
+    else if (n1) {
+        litros = 1000;  // 10%
+    } 
+    else {
+
+        litros = 0;     // 0% 
+    }
 }
 
 void processarLogicaNivel() {
